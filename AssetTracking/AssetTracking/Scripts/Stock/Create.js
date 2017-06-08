2b64c752207a427b8345a528999fad72434a24c6
Shibly Mohammad <sun8496@gmail.com>
2017-06-09 00:27:32 +0600

    stock

--- AssetTracking/AssetTracking/Scripts/Stock/Create.js
+++ AssetTracking/AssetTracking/Scripts/Stock/Create.js
@@ -1,4 +1,28 @@
-﻿window.onload = function() {
+﻿window.onload = function () {
+    $("#GeneralCategoryID").change(function () {
+        var generalcategoryId = $("#GeneralCategoryID").val();
+        if (generalcategoryId == "") {
+            $("#CategoryID").empty();
+            var option = "<option value=''>Select Category</option>";
+            $("#CategoryID").append(option);
+            return;
+        }
+        var jsonData = { generalcategoryId: generalcategoryId };
+        $.ajax({
+            url: "/GeneralCategory/GetCategoryByGeneralCategory",
+            type: "POST",
+            data: JSON.stringify(jsonData),
+            contentType: "application/json",
+            success: function (response) {
+                $("#CategoryID").empty();
+                $.each(response, function (key, value) {
+                    var optionText = "<option value='" + value.CategoryID + "'>" + value.CategoryName + "</option>";
+                    $("#CategoryID").append(optionText);
+                });
+            }
+
+        });
+    });
     $("#SubCategoryID").change(function() {
         var subcategoryId = $("#SubCategoryID").val();
         if (subcategoryId == "") {
