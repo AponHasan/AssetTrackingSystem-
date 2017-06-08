b5b9c281e56a804d7aea081baa00641f7537e71d
Shibly Mohammad <sun8496@gmail.com>
2017-06-09 01:00:57 +0600

    product update

--- AssetTracking/AssetTracking/Scripts/Product/Create.js
+++ AssetTracking/AssetTracking/Scripts/Product/Create.js
@@ -1,4 +1,28 @@
-﻿window.onload = function() {
+﻿window.onload = function () {
+    $("#GeneralCategoryID").change(function () {
+        var generalcategoryId = $("#GeneralCategoryID").val();
+        if (generalcategoryId == "") {
+            $("#CategoryID").empty();
+            var option = "<option value=''>Select Category</option>";
+            $("#CategoryID").append(option);
+            return;
+        }
+        var jsonData = { generalcategoryId: generalcategoryId };
+        $.ajax({
+            url: "/GeneralCategory/GetCategoryByGeneralCategory",
+            type: "POST",
+            data: JSON.stringify(jsonData),
+            contentType: "application/json",
+            success: function (response) {
+                $("#CategoryID").empty();
+                $.each(response, function (key, value) {
+                    var optionText = "<option value='" + value.CategoryID + "'>" + value.CategoryName + "</option>";
+                    $("#CategoryID").append(optionText);
+                });
+            }
+
+        });
+    });
     $("#CategoryID").change(function() {
         var categoryId = $("#CategoryID").val();
         if (categoryId == "") {
